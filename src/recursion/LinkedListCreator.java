eca59c589a90bc90c2398df994c71af40cc449dc
RicardoXu <1641137052@qq.com>
2019-03-30 21:37:18 +0800

    Initial commit

---
+++ src/recursion/LinkedListCreator.java
@@ -0,0 +1,10 @@
+package recursion;
+
+/**
+ * Created with IDEA
+ * author:RicardoXu
+ * Date:2019/3/30
+ * Time:21:19
+ */
+public class LinkedListCreator {
+}
---
+++ src/recursion/Node.java
@@ -0,0 +1,10 @@
+package recursion;
+
+/**
+ * Created with IDEA
+ * author:RicardoXu
+ * Date:2019/3/30
+ * Time:21:21
+ */
+public class Node {
+}
