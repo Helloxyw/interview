d27d7ca6ee814c6125a98a2296939ff77173c023
RicardoXu <1641137052@qq.com>
2019-03-31 12:49:49 +0800

    递归链表

--- src/recursion/LinkedListCreator.java
+++ src/recursion/LinkedListCreator.java
@@ -1,5 +1,7 @@
 package recursion;
 
+import java.util.List;
+
 /**
  * Created with IDEA
  * author:RicardoXu
@@ -7,4 +9,24 @@ package recursion;
  * Time:21:19
  */
 public class LinkedListCreator {
+
+    /**
+     * Creates a linked list
+     * @param data
+     * @return
+     */
+    public Node createLinkedList(List<Integer> data){
+        if(data.isEmpty()){
+            return null;
+        }
+        Node firstNode = new Node(data.get(0));
+        Node headOfSublist = createLinkedList(data.subList(1,data.size()));
+        firstNode.setNext(headOfSublist);
+        return firstNode;
+    }
+
+
+    public static void main(String []args){
+
+    }
 }
